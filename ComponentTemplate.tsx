78396fb5c56b1951bcc9be9139aefbecd171e382
Kent Claudio <rikysmile33@gmail.com>
2021-06-21 17:41:27 -0700

    Created ComponentTemplate TSX file
    Use as reference when creating new React components

---
+++ ComponentTemplate.tsx
@@ -0,0 +1,16 @@
+// third-party libraries
+
+// components
+
+// css
+
+// replace "App" with desired function component name
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
