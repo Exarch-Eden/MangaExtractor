1c81ede9abd9fd7b1e064963754a572d87404fa4
Kent Claudio <rikysmile33@gmail.com>
2021-06-24 00:13:15 -0700

    Updated manga-related types and interfaces

--- src/types/manga.ts
+++ src/types/manga.ts
@@ -1,1 +1,15 @@
-export {}
+export interface Book {
+  id: string;
+  title?: string;
+  cover?: string;
+  artist?: string;
+  pages?: Pages;
+  thumbnails?: Pages;
+}
+
+export type Pages = string[];
+
+export interface SessionData {
+  id: string;
+  pages: Pages;
+}
