1a2ae4d394237dc32833cf064920dfdcc5b67c71
Kent Claudio <rikysmile33@gmail.com>
2021-07-05 18:32:53 -0700

    Updated index with Store Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 // third-party libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 // components
-import App from './App';
+import App from "./App";
+import Store from "./redux/store";
 
 // css
-import './styles/index.css';
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
